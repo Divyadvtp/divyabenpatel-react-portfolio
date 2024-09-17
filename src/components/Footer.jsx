bb09e526d751e0252d8e0626e07cfa0be109edbc
Divyadvtp <tailord662@gmail.com>
2024-09-17 14:09:25 -0400

    changed footer element position for spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,9 @@ export default function Footer({ pageRefs }) {
           <a  href="https://www.linkedin.com/in/divyaben-patel-8428b9110/" target="_blank" rel="noopener noreferrer"><img src="img/LinkedIn.png" alt="LinkedIn" /></a>
           </div>
 
-        <div className="copyright inner-item">&copy;Copyright 2024. Made by <a className="footerName" rel="noreferrer" target="_blank" href="https://divyadvtp.github.io/divyabenpatel-react-portfolio/">Divyaben Patel</a></div>
+        <div className="copyright inner-item">&copy;Copyright 2024.
+          <div> Made by <a className="footerName" rel="noreferrer" target="_blank" href="https://divyadvtp.github.io/divyabenpatel-react-portfolio/">Divyaben Patel</a></div>
+        </div>
 
       </div>
     </footer>
